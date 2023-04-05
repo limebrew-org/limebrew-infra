9f3ca7a1ef79cc89018e78dcf7fc935945ae51cb
deepraj1729 <deeprajbaidyaofficial@gmail.com>
2023-04-06 01:08:05 +0530

    :zap: Modified file structure based on projects

--- infra/artifact_registry/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "registry_outputs" {
-  value = module.artifact_registry
-}
--- infra/artifact_registry/provider.tf
+++
@@ -1,7 +0,0 @@
-#TODO: Initialize Google provider
-provider "google" {
-  credentials = file(var.SERVICE_ACCOUNT_JSON)
-  project     = var.PROJECT_ID
-  region      = var.REGION
-  zone        = var.ZONE
-}
